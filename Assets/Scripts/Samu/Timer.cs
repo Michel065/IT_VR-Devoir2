b06ab2500f2fa47fb6d9e0e8232553efc68d50f5
samidete <samidete@gmail.com>
2026-02-06 16:05:55 -0500

    correction timer et teleportation

--- Assets/Scripts/Samu/Timer.cs
+++ Assets/Scripts/Samu/Timer.cs
@@ -4,15 +4,21 @@ using TMPro;
 public class VRTimer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
-    public float startTime = 60f;
+    public float startTime;
     private float currentTime;
     private bool isRunning = false;
+    public Transform teleportTarget;
+    Rigidbody rb;
+
+    public string sceneToLoad;
 
     void Start()
     {
+        ResetTimer();
         currentTime = startTime;
         UpdateDisplay();
         StartTimer();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -20,11 +26,14 @@ public class VRTimer : MonoBehaviour
         if (!isRunning) return;
 
         currentTime -= Time.deltaTime;
+        Debug.Log(currentTime);
         currentTime = Mathf.Max(currentTime, 0);
         UpdateDisplay();
 
+        
         if (currentTime <= 0)
         {
+            Debug.Log("Timer arrivé à zéro");
             isRunning = false;
             OnTimerEnd();
         }
@@ -33,7 +42,7 @@ public class VRTimer : MonoBehaviour
     void UpdateDisplay()
     {
         int minutes = Mathf.FloorToInt(currentTime / 60);
-        int seconds = Mathf.FloorToInt(currentTime % 60);
+        int seconds = Mathf.FloorToInt((currentTime) % 60);
         timerText.text = $"{minutes:00}:{seconds:00}";
     }
 
@@ -53,9 +62,27 @@ public class VRTimer : MonoBehaviour
         UpdateDisplay();
     }
 
-    void OnTimerEnd()
+    void OnTimerEnd() 
+    { 
+        Debug.Log("Timer terminé !");
+        // vibration, son, événement, etc. 
+        if (teleportTarget == null || rb == null) return;
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.position = teleportTarget.position;
+        rb.rotation = teleportTarget.rotation;
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("⏰ Timer terminé !");
-        // vibration, son, événement, etc.
+        if (other.CompareTag("Player"))
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.position = teleportTarget.position;
+            rb.rotation = teleportTarget.rotation;
+        }
     }
 }
+
